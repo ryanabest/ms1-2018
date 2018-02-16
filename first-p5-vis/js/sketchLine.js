4b22230be75f47fbc7c2e062dfc30fff63c09905
Ryan Best <ryanbest@Ryans-MacBook-Pro.local>
2018-02-15 22:55:55 -0500

    add line sketch

--- first-p5-vis/js/sketchLine.js
+++ first-p5-vis/js/sketchLine.js
@@ -1,6 +1,6 @@
 var canvas;
 var txt;
-let margin = 0.1;
+let margin = 0.075;
 let cnvW  = window.innerWidth-10;
 let cnvH = window.innerHeight-35;
 let x1 = cnvW*margin;
@@ -11,26 +11,28 @@ let y2 = cnvH-(cnvH*margin);
 let autoPlay = true;
 
 function preload() {
-  aY =   loadJSON('../assets/aggYear.json');
-  aC =   loadJSON('../assets/aggCountry.json');
-  aYC =  loadJSON('../assets/aggYearCountry.json');
-  aYL =  loadJSON('../assets/aggYearClassification.json');
-  aYCL = loadJSON('../assets/aggYearCountryClassification.json');
-  robotoFont = loadFont('../typeface/RobotoCondensed-Bold.ttf')
+  aY =         loadJSON('ms1-2018/first-p5-vis/assets/aggYear.json');
+  aC =         loadJSON('ms1-2018/first-p5-vis/assets/aggCountry.json');
+  aYC =        loadJSON('ms1-2018/first-p5-vis/assets/aggYearCountry.json');
+  aYL =        loadJSON('ms1-2018/first-p5-vis/assets/aggYearClassification.json');
+  aYCL =       loadJSON('ms1-2018/first-p5-vis/assets/aggYearCountryClassification.json');
+  robotoFont = loadFont('ms1-2018/first-p5-vis/typeface/RobotoCondensed-Bold.ttf')
 }
 
 function setup() {
+  console.log(aY);
   textFont(robotoFont);
   canvas = createCanvas(cnvW,cnvH);
   background(0);
   // countries
   countryNumber = Object.keys(aC['object_count_rank']).length;
   countries = Object.keys(aC['object_count'])
-  // create variable that will control year //
-  x = aY['acq_year'][0];
-  // determine maximum year in data set //
+  // determine minimum and maximum year //
   maxYearIndex = Object.keys(aY['acq_year']).length-1
+  minYear = aY['acq_year'][0]
   maxYear = aY['acq_year'][Object.keys(aY['acq_year'])[Object.keys(aY['acq_year']).length-1]-1]
+  // create variable that will control year //
+  x = minYear;
   // determine maximum value for country + year //
   maxYearCountryCount = 0;
   for (yc in Object.keys(aYC['object_cum_count'])) {
@@ -39,7 +41,7 @@ function setup() {
       maxYearCountryIndex = yc;
     }
   }
-
+  // stopLoop();
   // axes
   fill(255);
 }
@@ -47,28 +49,38 @@ function setup() {
 function draw() {
   drawYear();
   drawAxes();
-  drawCountries();
+  // drawCountries();
   txt = cnvH/1.5;
   // drawYearDot();
   drawYearCountryDot();
+  drawYearCountryLine();
+}
+
+function stopLoop() {
+  if (maxYear===floor(x)) {
+    noLoop();
+  } else {
+    loop();
+  }
 }
 
 function drawYear() {
   background(0);
   fill(255,50);
+  noStroke();
   textAlign(CENTER,CENTER);
   textSize(txt);
-  text(floor(x),cnvW/2,cnvH/2-50);
+  text(floor(x),cnvW/2,cnvH/2-75);
   if (ceil(x) <= maxYear) {
     if (autoPlay) {
-      x += 0.3;
+      x += 0.5;
     }
   }
 }
 
 function drawAxes() {
   stroke(255);
-  strokeWeight(10);
+  strokeWeight(3);
   line(x1,y1,x1,y2);
   line(x1,y2,x2,y2);
   noStroke();
@@ -78,10 +90,10 @@ function drawCountries() {
   for (c in countries) {
     let country = countries[c]
     let countryRank = aC['object_count_rank'][country]
-    let ctY = map(countryRank,1,countryNumber,y1+20,y2-20)
+    let ctX = map(countryRank,1,countryNumber,(cnvW*margin)+50,(cnvW-(cnvW*margin))-20)
     fill(255);
     textSize(24);
-    text(country,(cnvW*margin)/2,ctY)
+    text(country,ctX,cnvH-(cnvH*margin)+50)
   }
 }
 
@@ -91,7 +103,7 @@ function drawYearDot() {
     let yr = aY['acq_year'][y];
     if (yr === floor(x)) {
       let yrCount = aY['object_cum_count'][y]
-      let yrY = map(yrCount,maxCount,0,y1+20,y2-20);
+      let yrY = map(yrCount,maxCount,0,y1,y2);
       fill(255);
       ellipse(x1+200,yrY,50);
     }
@@ -105,14 +117,41 @@ function drawYearCountryDot() {
     if (yr === floor(x)) {
       let yrCountry = aYC['country'][y]
       let countryRank = aC['object_count_rank'][yrCountry]
+      let ctX = map(yr,minYear,maxYear,(cnvW*margin)+20,(cnvW-(cnvW*margin))-20)
       let yrCountryCount = aYC['object_cum_count'][y]
-      let ctX = map(yrCountryCount,0,maxYearCountryCount,(cnvW*margin),(cnvW-(cnvW*margin))-20)
-      // let ctX = map(countryRank,1,countryNumber,(cnvW*margin)+50,(cnvW-(cnvW*margin))-20)
-      let ctY = map(countryRank,1,countryNumber,y1+20,y2-20);
+      let ctY = map(yrCountryCount,maxYearCountryCount,0,y1+10,y2-10);
       fill(255);
-      ellipse(ctX,ctY,50);
+      ellipse(ctX,ctY,15);
       // console.log(yr);
       // console.log(yrCountryCount);
+    }
+  }
+}
+
+function drawYearCountryLine() {
+  for (c in countries) {
+    prevYears = []
+    let country = countries[c]
+    for (y in aYC['acq_year']) {
+      let yr = aYC['acq_year'][y];
+      if (yr <= floor(x) && aYC['country'][y] === country) {
+        prevYears.push({
+          year: yr,
+          object_cum_count: aYC['object_cum_count'][y]
+        })
+      }
+    }
+    if (prevYears.length>1) {
+      for (i=1;i<prevYears.length;i++) {
+        // console.log(prevYears[i-1]['object_cum_count'])
+        let linex1 = map(prevYears[i-1]['year'],minYear,maxYear,(cnvW*margin)+20,(cnvW-(cnvW*margin))-20);
+        let linex2 = map(prevYears[i]['year'],minYear,maxYear,(cnvW*margin)+20,(cnvW-(cnvW*margin))-20);
+        let liney1 = map(prevYears[i-1]['object_cum_count'],maxYearCountryCount,0,y1+10,y2-10);
+        let liney2 = map(prevYears[i]['object_cum_count'],maxYearCountryCount,0,y1+10,y2-10);
+        stroke(255,75);
+        strokeWeight(2);
+        line(linex1,liney1,linex2,liney2);
+      }
     }
   }
 }
@@ -132,7 +171,7 @@ function mousePressed() {
 function keyPressed() {
   if (autoPlay === false && keyCode === LEFT_ARROW) {
     x = floor(x) - 1
-  } else if (autoPlay === false && keyCode === RIGHT_ARROW) {
+  } else if (autoPlay === false && keyCode === RIGHT_ARROW && floor(x) < maxYear) {
     x = floor(x) + 1
   }
 }
@@ -141,7 +180,6 @@ window.onresize = function() {
   let w = window.innerWidth;
   let h = window.innerHeight;
   canvas.size(w,h);
-  background(0);
   width = w;
   height = h;
 }
