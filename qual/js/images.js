92a6c038983a4905eace90c8ea4cfb58049cc79b
ryanabest <ryanabest@gmail.com>
2018-03-12 19:19:03 -0400

    add file path variables

--- qual/js/images.js
+++ qual/js/images.js
@@ -1,7 +1,7 @@
 // let filePath = '../assets/' // Local Testing
-let filePath = '/ms1-2018/qual/assets/' // GitHub Pages
+let filePathImages = '/ms1-2018/qual/assets/' // GitHub Pages
 
-$.getJSON(filePath + 'metObjectsVanGogh.json', function(data) {
+$.getJSON(filePathImages + 'metObjectsVanGogh.json', function(data) {
   // console.log(Object.keys(data['image']).length);
   for (let i=0;i<Object.keys(data['image']).length;i++) {
     let image = data['image'][i]
