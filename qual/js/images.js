d8135109452939e277c54ad2f091f80b4ba815a2
ryanabest <ryanabest@gmail.com>
2018-03-13 12:01:54 -0400

    load web large images, not original

--- qual/js/images.js
+++ qual/js/images.js
@@ -7,6 +7,7 @@ $.getJSON(filePathImages + 'metObjectsVanGogh.json', function(data) {
     let image = data['image'][i]
     // let imagePath = 'assets/Images/' + image;
     let imageURL = data['image_url'][i]
+    imageURL = imageURL.replace("original","web-large")
     let imageHTML = "<img src='"+imageURL+"' class='start-stop' id='"+image+"'/>"
     $('.images').append(imageHTML);
   }
