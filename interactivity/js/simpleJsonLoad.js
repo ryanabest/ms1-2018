e5d24a85e9512a8922d4a27529f8b2667b79895c
ryanabest <ryanabest@gmail.com>
2018-04-13 13:26:31 -0400

    try with d3 v4

--- interactivity/js/simpleJsonLoad.js
+++ interactivity/js/simpleJsonLoad.js
@@ -1,21 +1,8 @@
-let dataPromise = d3.json(filePath+'metObjectsVanGogh.json').then(function(paths) {
-  // console.log(data);
-  let files = [];
-  let promises = [];
+let dataPromise = d3.json(filePath+'metObjectsVanGogh.json',function(error,paths) {
   for (let p=0;p<Object.keys(paths.image).length;p++) {
     let JSONPath = filePath + "jsonLine2" + paths.image[p].split(".")[0] + ".json";
-    files.push(JSONPath);
+    d3.json(JSONPath,function(error,proms) {
+      console.log(proms);
+    })
   }
-  files.forEach(function(url){
-    promises.push(d3.json(url));
-  })
-
-  console.log(files);
-  console.log(promises);
-});
-
-
-
-dataPromise.catch(function(reason) {
-  console.log(reason);
 });
