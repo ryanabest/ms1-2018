6028f5ac839a8f987349d2674058c8dd8e0006e8
ryanabest <ryanabest@gmail.com>
2018-04-13 13:31:36 -0400

    back to d3v5 with LINE caps

--- interactivity/js/simpleJsonLoad.js
+++ interactivity/js/simpleJsonLoad.js
@@ -1,8 +1,22 @@
-let dataPromise = d3.json(filePath+'metObjectsVanGogh.json',function(error,paths) {
+let dataPromise = d3.json(filePath+'metObjectsVanGogh.json').then(function(paths) {
+  // console.log(data);
+  let files = [];
+  let promises = [];
   for (let p=0;p<Object.keys(paths.image).length;p++) {
-    let JSONPath = filePath + "jsonLine2" + paths.image[p].split(".")[0] + ".json";
-    d3.json(JSONPath,function(error,proms) {
-      console.log(proms);
-    })
+    let JSONPath = filePath + "jsonLINE2" + paths.image[p].split(".")[0] + ".json";
+    files.push(JSONPath);
   }
+  files.forEach(function(url){
+    promises.push(d3.json(url));
+  })
+
+  Promise.all(promises).then(function(proms) {
+    console.log(proms);
+  })
+});
+
+
+
+dataPromise.catch(function(reason) {
+  console.log(reason);
 });
