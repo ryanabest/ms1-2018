ef19bd1a6fcf11c32a6f4ac0be507619840994cd
ryanabest <ryanabest@gmail.com>
2018-04-13 13:19:07 -0400

    print files and promises

--- interactivity/js/simpleJsonLoad.js
+++ interactivity/js/simpleJsonLoad.js
@@ -1,6 +1,20 @@
-let dataPromise = d3.json(filePath+'metObjectsVanGogh.json').then(function(data) {
-  console.log(data);
+let dataPromise = d3.json(filePath+'metObjectsVanGogh.json').then(function(paths) {
+  // console.log(data);
+  let files = [];
+  let promises = [];
+  for (let p=0;p<Object.keys(paths.image).length;p++) {
+    let JSONPath = filePath + "jsonLine2" + paths.image[p].split(".")[0] + ".json";
+    files.push(JSONPath);
+  }
+  files.forEach(function(url){
+    promises.push(d3.json(url));
+  })
+
+  console.log(files);
+  console.log(promises);
 });
+
+
 
 dataPromise.catch(function(reason) {
   console.log(reason);
